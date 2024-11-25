2e1e3f038152084ed5f7330a44cbbebf18220bb2
Jackie-LM <lmx1342@gmail.com>
2024-11-25 15:12:28 +0800

    fix bug

--- akechi-portfolio/src/App.js
+++ akechi-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import TitlePage from "./pages/TitlePage";
 import PortfolioPage from "./pages/PortfolioPage";
@@ -8,10 +8,10 @@ import PortfolioPage from "./pages/PortfolioPage";
 const App = () => {
   return (
     <Router basename="/AkechiPortfolio">
-      <Switch>
+      <Routes >
         <Route path="/" exact component={TitlePage} />
         <Route path="/portfolio" exact component={PortfolioPage} />
-      </Switch>
+      </Routes >
     </Router>
   );
 };
