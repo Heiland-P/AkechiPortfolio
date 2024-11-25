bdd2f2e3f12ccb1d93c340998ab94f81abd11241
Jackie-LM <lmx1342@gmail.com>
2024-11-25 15:07:31 +0800

    add basename

--- akechi-portfolio/src/App.js
+++ akechi-portfolio/src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import TitlePage from "./pages/TitlePage";
 import PortfolioPage from "./pages/PortfolioPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/">
-      <Route path="AkechiPortfolio" element={<TitlePage />} />
-      <Route path="portfolio" element={<PortfolioPage />} />
-    </Route>
-  )
-);
-
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Router basename="/AkechiPortfolio">
+      <Switch>
+        <Route path="/" exact component={TitlePage} />
+        <Route path="/portfolio" exact component={PortfolioPage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
