a6a226b38f6930e573a276535800eb1f4d77a357
Heiland-P <lmx1342@gmail.com>
2024-11-28 20:45:50 +0800

    backend url update

--- akechi-portfolio/src/Config.js
+++ akechi-portfolio/src/Config.js
@@ -1,6 +1,6 @@
 const config = {
     // backendUrl: "http://localhost:3000", // Local
-    backendUrl: "akechi-portfolio-backend-btslwowwf-jackies-projects-007eb301.vercel.app", 
+    backendUrl: "https://akechi-portfolio-backend.vercel.app", 
 
   };
   
